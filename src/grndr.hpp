2112e3a413291f6ec82d9531210bfa2aad28ec60
bggd <gameovera@live.com>
2017-12-12 16:03:22 +0900

    Add src/grndr.hpp

---
+++ src/grndr.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include "ogl/ogl.hpp"
+#include "ogl/vbo.hpp"
+#include "ogl/ibo.hpp"
+#include "ogl/texture_2d.hpp"
+#include "ogl/shader_glsl.hpp"
+
+#include "pipeline_spec.hpp"
+#include "renderer.hpp"
+
+
+#ifdef GAME_RENDERER_IMPLEMENTATION
+#include "ogl/vbo.cpp"
+#include "ogl/ibo.cpp"
+#include "ogl/texture_2d.cpp"
+#include "ogl/shader_glsl.cpp"
+
+#include "pipeline_spec.cpp"
+#include "renderer.cpp"
+#endif
