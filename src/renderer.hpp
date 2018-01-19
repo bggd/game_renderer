aba5879415dc8015f14f98e65401bbe7864b39c7
bggd <gameovera@live.com>
2018-01-19 20:50:22 +0900

    Change API: set_pipeline -> bind_pipeline, bind_vertex_attributes

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -2,14 +2,15 @@ namespace grndr {
 
 struct Renderer {
 
-  PipelineSpec spec;
+  PipelineSpec* current_spec = nullptr;
   std::vector<ogl::VBO> vbo_list;
   ogl::IBO ibo;
 
-  void set_pipeline(const PipelineSpec& spec);
-  void set_uniform_matrix4(const PipelineSpec::Shader::Uniform& u, const GLfloat* elements);
   void set_vbo(uint8_t idx, ogl::VBO vbo);
   void set_ibo(ogl::IBO ibo);
+  void bind_pipeline(PipelineSpec* spec);
+  void bind_vertex_attributes(const PipelineSpec* spec, bool use_ibo=false);
+  void set_uniform_matrix4(const PipelineSpec::Shader::Uniform& u, const GLfloat* elements);
 
   void draw_triangles(uint16_t first, uint16_t count, bool use_ibo=false);
 
